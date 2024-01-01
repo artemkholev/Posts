581f11498c3fd1c421221b25b3dd9cfc7c7a9aa1
Artem <artemkholev@yandex.ru>
2024-01-01 17:34:18 +0300

    edit main page

--- src/pages/mainPage/MainPage.vue
+++ src/pages/mainPage/MainPage.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <div class="generalInfo">
         <p>Tesk for enter posts and info about post/user...</p>
-        <img src="../../../public/RdePFnMh3TuIA2jHLFcPIw0VL04_bSPlRVZMnVRadMdCEiXEXO-whGWjHShtIVlcZSaHaP2t0QDsyYBCu3OnShoHZdfKvU4yoexZyZXGIuuvAD4XXSPJfmCE8mpHAiax1ClnO0NZ57ds_rYaM5a0V1-0qoL436MEgkFQmUCxXRbxA2AXFeBrcSBQ8Hxj6MFXmnNxJSILWR6eY_a4wtjgIvXRT_VGbQV6K2Hta1GJYRQok0tXOWtuenxZFvK.jpg" alt="">
+        <img src="../../../public/dog.jpg" alt="">
       </div>
   </div>
 </template>
